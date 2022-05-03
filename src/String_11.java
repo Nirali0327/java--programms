2ebcda76dcdcb64823dc167020ccf9062938e0bf
Nirali0327 <nirali0327@gmail.com>
2022-05-03 21:58:16 +0100

    second commit

--- src/String_11.java
+++ src/String_11.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-
 public class String_11 {
-   //	WAP to enter any String and count total number of 'a' in that String.
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(" Enter the String");
-        String result = scanner.next();
-        int value = 0;
-        for (int i = 0; i < result.length(); i++) {
-            if (result.charAt(i) == ' ') ;
-            value++;
+     //	WAP to enter any String and count total number of 'a' in that String.
+    public static void main(String args[])
+    {
+        int c = 0;
+        String St = "'I always like to eat chocolate cakes and chocolate ice cream'";
+        char letter = 'a';
+        for (int i = 0; i < St.length(); i++)
+        {
+            if (St.charAt(i) == letter) // ---- inbuilt St.charAt(i) method to count character in string'.
+                c++;
         }
-           System.out.println(" total character " + value);
-    }
-   }
+
+        System.out.println("Total number of "+letter+ " is " + c + "  in String  " + St);
+        }
+}
